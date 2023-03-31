cacb4e6f16131b78d5ecd4b9903f495554f39909
gerasiovM <maxim.gerasiov@gmail,com>
2023-04-01 01:00:00 +0300

    Created ParticleGroup class that is the base class for any object consisting of multiple particles.

---
+++ src/main/java/net/gerasiov/particleapi/geometry/ParticleGroup.java
@@ -0,0 +1,49 @@
+package net.gerasiov.particleapi.geometry;
+
+import net.gerasiov.particleapi.ParticleAPI;
+import net.gerasiov.particleapi.particles.ParticlePoint;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.List;
+
+public class ParticleGroup {
+    private List<ParticlePoint> particles;
+
+
+    public ParticleGroup(List<ParticlePoint> particles) {
+        this.particles = particles;
+    }
+
+
+    public List<ParticlePoint> getParticles() {
+        return particles;
+    }
+
+    public void setParticle(int index, ParticlePoint particle) {
+        particles.set(index, particle);
+    }
+
+    public void spawn() {
+        for (ParticlePoint particle: particles) {
+            particle.spawn();
+        }
+    }
+
+    /**
+     * Spawn all particles in group with a period between each spawn.
+     *
+     * @param delay Delay before the first spawn in ticks.
+     * @param period Period between every spawn in ticks.
+     */
+    public void spawnWithDelays(int delay, int period) {
+        new BukkitRunnable() {
+            int index = 0;
+
+            @Override
+            public void run() {
+                particles.get(index).spawn();
+                index++;
+            }
+        }.runTaskTimerAsynchronously(ParticleAPI.getInstance(), delay, period);
+    }
+}
