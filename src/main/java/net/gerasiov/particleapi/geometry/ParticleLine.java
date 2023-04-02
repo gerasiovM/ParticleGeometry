4fc04741e28e4fce2a9ba38f2ffd9fa632bdb1ab
maxim <maxim.gerasiov@gmail.com>
2023-04-02 17:18:41 +0300

    Added a prototype for particle list creation in ParticleLine. This class will probably still not work, but it just might.

--- src/main/java/net/gerasiov/particleapi/geometry/ParticleLine.java
+++ src/main/java/net/gerasiov/particleapi/geometry/ParticleLine.java
@@ -1,13 +1,12 @@
 package net.gerasiov.particleapi.geometry;
 
-import net.gerasiov.particleapi.particles.DirectionalParticle;
-import net.gerasiov.particleapi.particles.DustParticle;
-import net.gerasiov.particleapi.particles.ParticlePoint;
+import net.gerasiov.particleapi.particles.*;
 import net.gerasiov.particleapi.particles.types.DirectionalParticleTypes;
 import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.util.Vector;
+import org.checkerframework.checker.units.qual.A;
 
 import java.util.*;
 
@@ -15,45 +14,61 @@ public class ParticleLine extends ParticleGroup {
     private Location startLocation;
     private Location endLocation;
     private double interval;
+    private ArrayList<ParticlePoint> particles;
+
+    private ArrayList<ParticlePoint> template;
 
 
     public ParticleLine(Location startLocation, Location endLocation, double interval, ParticlePoint particle) {
-        super(createList(startLocation, endLocation, interval, particle));
+        super(createList(startLocation, endLocation, calculateRealInterval(startLocation, endLocation, interval), particle));
         this.startLocation = startLocation;
         this.endLocation = endLocation;
-        this.interval = interval;
+        this.interval = calculateRealInterval(startLocation, endLocation, interval);
+        this.particles = createList(startLocation, endLocation, interval, particle);
+        ArrayList<ParticlePoint> template = new ArrayList<>();
+        template.add(particle);
+        this.template = template;
     }
 
-    private static List<ParticlePoint> createList(Location startLocation, Location endLocation, double interval, ParticlePoint particle) {
-        double startX = startLocation.getX();
-        double startY = startLocation.getY();
-        double startZ = startLocation.getZ();
-        double endX = endLocation.getX();
-        double endY = endLocation.getY();
-        double endZ = endLocation.getZ();
+    private static ArrayList<ParticlePoint> createList(Location startLocation, Location endLocation, double interval, ParticlePoint particle) {
+        double distance = endLocation.toVector().subtract(startLocation.toVector()).length();
+        double numberOfParticles = Math.round(distance / interval);
 
-        double distance = Math.sqrt(Math.pow(startX - endX, 2) + Math.pow(startY - endY, 2) + Math.pow(startZ - endZ, 2));
-        double realInterval = distance / Math.round(distance / interval);
-        List<ParticlePoint> particles = new ArrayList<>();
+        ArrayList<ParticlePoint> particles = new ArrayList<>();
 
-        Location particleLocation = particle.getLocation();
-        Particle particleType = particle.getType();
-        if (particle instanceof DirectionalParticle) {
-            Vector particleDirection = ((DirectionalParticle) particle).getDirection();
-            double particleSpeed = ((DirectionalParticle) particle).getSpeed();
-            for (double i = 0; i <= distance; i+= realInterval) {
-                DirectionalParticle newParticle = new DirectionalParticle(particleType, particleLocation, particleDirection, particleSpeed);
-                particles.add(newParticle);
-            }
-        }else if (particle instanceof DustParticle) {
-            Color particleColor = ((DustParticle) particle).getColor();
-            Color particleSecondaryColor = ((DustParticle) particle).getSecondaryColor();
-            float particleSize = ((DustParticle) particle).getSize();
-            for (double i = 0; i <= distance; i+= realInterval) {
-                DustParticle newParticle = new DustParticle(particleLocation, particleColor, particleSecondaryColor, particleSize);
-                particles.add(newParticle);
-            }
+        Vector directionVector = endLocation.toVector().subtract(startLocation.toVector());
+        directionVector.normalize();
+        Vector displacementVector = directionVector.clone().multiply(interval);
+
+        for (int i = 0; i < numberOfParticles; i++) {
+            Location newLocation = startLocation.clone().add(displacementVector.multiply(i));
+            ParticlePoint newParticle = particle.clone();
+            newParticle.setLocation(newLocation);
+            particles.add(newParticle);
         }
 
+        return particles;
+    }
+
+    public static double calculateRealInterval(Location startLocation, Location endLocation, double interval) {
+        double distance = endLocation.toVector().subtract(startLocation.toVector()).length();
+        double numberOfParticles = Math.round(distance / interval);
+        return distance / numberOfParticles;
+    }
+
+    public Location getStartLocation() {
+        return this.startLocation;
+    }
+
+    public Location getEndLocation() {
+        return this.endLocation;
+    }
+
+    public double getInterval() {
+        return this.interval;
+    }
+
+    public ArrayList<ParticlePoint> getTemplate() {
+        return this.template;
     }
 }
