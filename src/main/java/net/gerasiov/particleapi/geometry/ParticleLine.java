fd7a5998247e067c341f6e68526edf4ee818de43
maxim <maxim.gerasiov@gmail.com>
2023-04-05 18:39:30 +0300

    Finished ParticleLine class, along with implementing constructor that supports schemes.

--- src/main/java/net/gerasiov/particleapi/geometry/ParticleLine.java
+++ src/main/java/net/gerasiov/particleapi/geometry/ParticleLine.java
@@ -1,53 +1,32 @@
 package net.gerasiov.particleapi.geometry;
 
 import net.gerasiov.particleapi.particles.*;
-import net.gerasiov.particleapi.particles.types.DirectionalParticleTypes;
-import org.bukkit.Color;
+import net.gerasiov.particleapi.schemes.line.LineScheme;
 import org.bukkit.Location;
-import org.bukkit.Particle;
-import org.bukkit.util.Vector;
-import org.checkerframework.checker.units.qual.A;
-
-import java.util.*;
 
 public class ParticleLine extends ParticleGroup {
     private Location startLocation;
     private Location endLocation;
     private double interval;
-    private ArrayList<ParticlePoint> particles;
+    private ParticlePoint[] particles;
 
-    private ArrayList<ParticlePoint> template;
+    private LineScheme scheme;
+
 
 
     public ParticleLine(Location startLocation, Location endLocation, double interval, ParticlePoint particle) {
-        super(createList(startLocation, endLocation, calculateRealInterval(startLocation, endLocation, interval), particle));
+        super(new LineScheme(particle).createArray(startLocation, endLocation, calculateRealInterval(startLocation, endLocation, interval)));
         this.startLocation = startLocation;
         this.endLocation = endLocation;
         this.interval = calculateRealInterval(startLocation, endLocation, interval);
-        this.particles = createList(startLocation, endLocation, interval, particle);
-        ArrayList<ParticlePoint> template = new ArrayList<>();
-        template.add(particle);
-        this.template = template;
     }
 
-    private static ArrayList<ParticlePoint> createList(Location startLocation, Location endLocation, double interval, ParticlePoint particle) {
-        double distance = endLocation.toVector().subtract(startLocation.toVector()).length();
-        double numberOfParticles = Math.round(distance / interval);
-
-        ArrayList<ParticlePoint> particles = new ArrayList<>();
-
-        Vector directionVector = endLocation.toVector().subtract(startLocation.toVector());
-        directionVector.normalize();
-        Vector displacementVector = directionVector.clone().multiply(interval);
-
-        for (int i = 0; i < numberOfParticles; i++) {
-            Location newLocation = startLocation.clone().add(displacementVector.multiply(i));
-            ParticlePoint newParticle = particle.clone();
-            newParticle.setLocation(newLocation);
-            particles.add(newParticle);
-        }
-
-        return particles;
+    public ParticleLine(Location startLocation, Location endLocation, double interval, LineScheme scheme) {
+        super(scheme.createArray(startLocation, endLocation, interval));
+        this.startLocation = startLocation;
+        this.endLocation = endLocation;
+        this.interval = calculateRealInterval(startLocation, endLocation, interval);
+        this.scheme = scheme;
     }
 
     public static double calculateRealInterval(Location startLocation, Location endLocation, double interval) {
@@ -68,7 +47,24 @@ public class ParticleLine extends ParticleGroup {
         return this.interval;
     }
 
-    public ArrayList<ParticlePoint> getTemplate() {
-        return this.template;
+    public LineScheme getScheme() {
+        return this.scheme;
+    }
+
+    public void setStartLocation(Location startLocation) {
+        this.startLocation = startLocation;
+        this.interval = calculateRealInterval(this.startLocation, this.endLocation, this.interval);
+        this.particles = this.scheme.createArray(this.startLocation, this.endLocation, this.interval);
+    }
+
+    public void setEndLocation(Location endLocation) {
+        this.endLocation = endLocation;
+        this.interval = calculateRealInterval(this.startLocation, this.endLocation, this.interval);
+        this.particles = this.scheme.createArray(this.startLocation, this.endLocation, this.interval);
+    }
+
+    public void setInterval(double interval) {
+        this.interval = calculateRealInterval(this.startLocation, this.endLocation, interval);
+        this.particles = this.scheme.createArray(this.startLocation, this.endLocation, this.interval);
     }
 }
