8e47a68396b4d811d5668f92733b0a0a5e35d87a
Amir <50082485+erxson@users.noreply.github.com>
2023-05-13 15:16:21 +0300

    Update ParticleGroupSpawnEvent.java

--- src/main/java/net/gerasiov/particleapi/events/ParticleGroupSpawnEvent.java
+++ src/main/java/net/gerasiov/particleapi/events/ParticleGroupSpawnEvent.java
@@ -1,14 +1,14 @@
 package net.gerasiov.particleapi.events;
 
-import net.gerasiov.particleapi.geometry.ParticleGroup;
 import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import net.gerasiov.particleapi.geometry.ParticleGroup;
 
-public class ParticleGroupSpawnEvent extends org.bukkit.event.Event implements Cancellable {
-    private static final HandlerList handlers = new HandlerList();
-
+public class ParticleGroupSpawnEvent extends Event implements Cancellable {
     private final ParticleGroup particleGroup;
     private boolean cancelled = false;
+    private static final HandlerList handlers = new HandlerList();
 
     public ParticleGroupSpawnEvent(ParticleGroup particleGroup) {
         this.particleGroup = particleGroup;
@@ -17,7 +17,7 @@ public class ParticleGroupSpawnEvent extends org.bukkit.event.Event implements C
     public ParticleGroup getParticleGroup() {
         return particleGroup;
     }
-
+    
     @Override
     public HandlerList getHandlers() {
         return handlers;
@@ -25,7 +25,7 @@ public class ParticleGroupSpawnEvent extends org.bukkit.event.Event implements C
 
     public static HandlerList getHandlerList() {
         return handlers;
-    }
+    }   
 
     @Override
     public boolean isCancelled() {
@@ -33,8 +33,7 @@ public class ParticleGroupSpawnEvent extends org.bukkit.event.Event implements C
     }
 
     @Override
-    public void setCancelled(boolean cancelled) {
-        this.cancelled = cancelled;
+    public void setCancelled(boolean cancel) {
+        cancelled = cancel;
     }
 }
-
