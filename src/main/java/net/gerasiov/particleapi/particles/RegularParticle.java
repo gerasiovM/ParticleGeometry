b9d3301adc472f00803f202a52fd7c19361d02e1
Amir <50082485+erxson@users.noreply.github.com>
2023-05-13 14:31:04 +0300

    Update RegularParticle.java

--- src/main/java/net/gerasiov/particleapi/particles/RegularParticle.java
+++ src/main/java/net/gerasiov/particleapi/particles/RegularParticle.java
@@ -1,19 +1,19 @@
 package net.gerasiov.particleapi.particles;
 
-import net.gerasiov.particleapi.events.ParticleSpawnEvent;
+import java.util.Collection;
+import java.util.function.Predicate;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.entity.Entity;
+import org.bukkit.plugin.PluginManager;
 
-import java.util.Collection;
-import java.util.function.Predicate;
+import net.gerasiov.particleapi.events.ParticleSpawnEvent;
 
 public class RegularParticle {
-
     private final Particle type;
     private Location location;
-
 
     public RegularParticle(Particle type, Location location) {
         this.type = type;
@@ -33,22 +33,23 @@ public class RegularParticle {
     }
 
     public RegularParticle clone() {
-        return new RegularParticle(this.type, this.location);
+        return new RegularParticle(type, location);
     }
 
     public Collection<Entity> getNearbyEntities(double radiusX, double radiusY, double radiusZ) {
-        return this.location.getWorld().getNearbyEntities(this.location, radiusX, radiusY, radiusZ);
+        return location.getWorld().getNearbyEntities(location, radiusX, radiusY, radiusZ);
     }
 
     public Collection<Entity> getNearbyEntities(double radiusX, double radiusY, double radiusZ, Predicate<Entity> filter) {
-        return this.location.getWorld().getNearbyEntities(this.location, radiusX, radiusY, radiusZ, filter);
+        return location.getWorld().getNearbyEntities(location, radiusX, radiusY, radiusZ, filter);
     }
 
     public void spawn() {
-        ParticleSpawnEvent particleSpawnEvent = new ParticleSpawnEvent(this);
-        Bukkit.getServer().getPluginManager().callEvent(particleSpawnEvent);
+        ParticleSpawnEvent event = new ParticleSpawnEvent(this);
+        PluginManager pluginManager = Bukkit.getServer().getPluginManager();
+        pluginManager.callEvent(event);
 
-        if (!particleSpawnEvent.isCancelled()) {
+        if (!event.isCancelled()) {
             location.getWorld().spawnParticle(type, location, 1);
         }
     }
