cfd52b8f894596d648965511581ca5453dc2aca1
Amir <50082485+erxson@users.noreply.github.com>
2023-05-13 15:50:14 +0300

    Update DustParticle.java

--- src/main/java/net/gerasiov/particleapi/particles/DustParticle.java
+++ src/main/java/net/gerasiov/particleapi/particles/DustParticle.java
@@ -1,43 +1,41 @@
 package net.gerasiov.particleapi.particles;
 
 import java.util.function.Predicate;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.Particle.DustOptions;
 import org.bukkit.Particle.DustTransition;
-import org.bukkit.entity.Entity;
 
 import net.gerasiov.particleapi.events.ParticleSpawnEvent;
 
-public class DustParticle extends RegularParticle{
+public class DustParticle extends RegularParticle {
     private Color color;
     private Color secondaryColor;
     private float size;
-
 
     /**
      * Creates a new {@link DustParticle} object with the specified parameters.
      *
      * @param location The location for the particle.
-     * @param color The color for the particle.
-     * @param size The size for the particle.
+     * @param color    The color for the particle.
+     * @param size     The size for the particle.
      */
     public DustParticle(Location location, Color color, float size) {
         super(Particle.REDSTONE, location);
         this.color = color;
-        this.secondaryColor = null;
         this.size = size;
     }
 
     /**
      * Creates a new {@link DustParticle} object with the specified parameters.
      *
-     * @param location The location for the particle.
-     * @param color The color for the particle.
-     * @param secondaryColor The secondary color for the particle, towards which the main color will transition.
-     * @param size The size for the particle.
+     * @param location        The location for the particle.
+     * @param color           The color for the particle.
+     * @param secondaryColor  The secondary color for the particle, towards which the main color will transition.
+     * @param size            The size for the particle.
      */
     public DustParticle(Location location, Color color, Color secondaryColor, float size) {
         super(Particle.DUST_COLOR_TRANSITION, location);
@@ -47,15 +45,15 @@ public class DustParticle extends RegularParticle{
     }
 
     public Color getColor() {
-        return this.color;
+        return color;
     }
 
     public Color getSecondaryColor() {
-        return this.secondaryColor;
+        return secondaryColor;
     }
 
     public float getSize() {
-        return this.size;
+        return size;
     }
 
     public void setColor(Color color) {
@@ -73,9 +71,9 @@ public class DustParticle extends RegularParticle{
     @Override
     public DustParticle clone() {
         if (getType() == Particle.REDSTONE) {
-            return new DustParticle(getLocation(), this.color, this.size);
+            return new DustParticle(getLocation(), color, size);
         }
-        return new DustParticle(getLocation(), this.color, this.secondaryColor, this.size);
+        return new DustParticle(getLocation(), color, secondaryColor, size);
     }
 
     @Override
@@ -86,10 +84,10 @@ public class DustParticle extends RegularParticle{
         if (!particleSpawnEvent.isCancelled()) {
             if (getType() == Particle.REDSTONE) {
                 DustOptions dustOptions = new DustOptions(color, size);
-                getLocation().getWorld().spawnParticle(Particle.REDSTONE, getLocation(), 1, dustOptions);
+                getLocation().getWorld().spawnParticle(getType(), getLocation(), 1, dustOptions);
             } else {
                 DustTransition dustTransition = new DustTransition(color, secondaryColor, size);
-                getLocation().getWorld().spawnParticle(Particle.DUST_COLOR_TRANSITION, getLocation(), 1, dustTransition);
+                getLocation().getWorld().spawnParticle(getType(), getLocation(), 1, dustTransition);
             }
         }
     }
